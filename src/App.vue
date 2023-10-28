eb9bfcaceec6076390db7e7b04e0f34fa0777555
Salah Ismail <S5239380@studenti.unige.it>
2023-10-28 21:23:04 +0200

    header and routing

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
-  <div class = "flex flex-col min-h-screen font-Roboto bg-weather-primary" >
+  <div class = "flex flex-col min-h-screen font-Roboto" >
+    <Header/>
     <RouterView/>
     <site-navigation/>
 
   </div>
 </template>
-<script setup>
-
+<script>
+import Header from "./components/Header.vue";
 import { RouterView}  from "vue-router";
 import SiteNavigation from "@/components/SiteNavigation.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header
+  },
+};
 </script>
 
 <style lang="scss" scoped>
---
+++ src/views/Assignment1.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>This is assignment 1</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Assignment1',
+    // Your component's logic here
+}
+</script>
+
+<style scoped>
+/* Your component's styles here */
+</style>
